346299cfff2eca1589c4d1693e7c04759222fa13
Wedaad <wedaadh2@gmail.com>
2022-11-04 23:54:32 +0000

    add: login url

--- awm_locator_app/awm_locator_app/urls.py
+++ awm_locator_app/awm_locator_app/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
+from location_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html'), name='home')
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
+    path('login/', views.user_login, name='login')
 ]
