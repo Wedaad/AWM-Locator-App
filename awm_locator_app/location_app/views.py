49b79d73ebb5f0a3535b2e4b3b63ac2c547361ca
Wedaad <wedaadh2@gmail.com>
2022-11-04 23:54:16 +0000

    add: login template and view

--- awm_locator_app/location_app/views.py
+++ awm_locator_app/location_app/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
+def user_login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                # messages.info(request, f"You are now logged in as {username}.")
+                return redirect("/menu")
+            else:
+                return redirect('/')
+        else:
+            return redirect('/')
+    else:
+        form = AuthenticationForm()
+        return render(request=request, template_name="user_forms/login.html", context={"login_form": form})
