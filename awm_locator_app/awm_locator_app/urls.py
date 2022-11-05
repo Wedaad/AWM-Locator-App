54a2d8964df1a7febd4f4d45d36774bb3fcfc1f7
Wedaad <wedaadh2@gmail.com>
2022-11-05 00:06:14 +0000

    add: url for register template

--- awm_locator_app/awm_locator_app/urls.py
+++ awm_locator_app/awm_locator_app/urls.py
@@ -21,5 +21,6 @@ from location_app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='index.html'), name='home'),
-    path('login/', views.user_login, name='login')
+    path('login/', views.user_login, name='login'),
+    path('register/', views.new_user_registration, name='register_user'),
 ]
