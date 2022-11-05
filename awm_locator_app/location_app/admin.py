d784b7b014b7bd25251f6805090c5407cdd4c204
Wedaad <wedaadh2@gmail.com>
2022-11-05 23:09:34 +0000

    add: registered UserProfile model to django admin

--- awm_locator_app/location_app/admin.py
+++ awm_locator_app/location_app/admin.py
@@ -1,6 +1,7 @@
 # Register your models here.
 
 from django.contrib.gis import admin
-from .models import WorldBorder
+from .models import WorldBorder, UserProfile
 
 admin.site.register(WorldBorder, admin.OSMGeoAdmin)
+admin.site.register(UserProfile, admin.OSMGeoAdmin)
